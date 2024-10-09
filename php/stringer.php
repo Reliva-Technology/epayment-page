493dbb05c42360cc13e475a2db2e47acfcefe1dd
Fadli Saad <fadlisaad@outlook.com>
2024-10-09 12:09:09 +0800

    Remove debug

--- php/stringer.php
+++ php/stringer.php
@@ -37,30 +37,8 @@ class StringerController
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 
         $response = curl_exec($ch);
-		print_r($response);
-		die();
-        $body = null;
-	    // error
-	    if (!$response) {
-	        $body = curl_error($ch);
-	        // HostNotFound, No route to Host, etc  Network related error
-	        $http_status = -1;
-	    } else {
-	       //parsing http status code
-	        $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		curl_close($ch);
 
-	        if (!is_null($header)) {
-	            $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-
-	            $header = substr($response, 0, $header_size);
-	            $body = substr($response, $header_size);
-	        } else {
-	            $body = $response;
-	        }
-	    }
-
-	    curl_close($ch);
-
-	    return $body;
+	    return $response;
 	}
 }
