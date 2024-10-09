234ff80630366a3c7bfbab372ebde44ac1ca657b
Fadli Saad <fadlisaad@outlook.com>
2024-10-09 14:58:13 +0800

    echo response

--- php/stringer.php
+++ php/stringer.php
@@ -37,29 +37,8 @@ class StringerController
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 
         $response = curl_exec($ch);
-		
-        $body = null;
-	    // error
-	    if (!$response) {
-	        $body = curl_error($ch);
-	        // HostNotFound, No route to Host, etc  Network related error
-	        $http_status = -1;
-	    } else {
-	       //parsing http status code
-	        $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-	        if (!is_null($header)) {
-	            $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-
-	            $header = substr($response, 0, $header_size);
-	            $body = substr($response, $header_size);
-	        } else {
-	            $body = $response;
-	        }
-	    }
-
 	    curl_close($ch);
 
-	    echo $body;
+		echo $response;
 	}
 }
