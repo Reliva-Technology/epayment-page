e194f4b9e5fc21762c611d16d08ca86eb4763bf2
Fadli Saad <fadlisaad@gmail.com>
2024-11-25 10:27:55 +0800

    Replace MIGS to MPGS

--- php/payment.php
+++ php/payment.php
@@ -149,7 +149,7 @@ class Payment
             case 'fpx1':
                 $payment_mode = 'FPX (Corporate)';
                 break;
-            case 'migs':
+            case 'mpgs':
                 $payment_mode = 'Kad Kredit/Debit';
                 break;
             default:
